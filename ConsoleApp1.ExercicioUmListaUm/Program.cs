48b56d1296199a8fabf05608bf73a3ceecc73f94
joaog <joaog@DESKTOP-ECAA0L7>
2022-02-18 16:45:11 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1.ExercicioUmListaUm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1.ExercicioUmListaUm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imobiliária Imóbilis");
+            Console.WriteLine("Area de um retangulo b x h (base x altura)");
+            while (true)
+            {
+                Console.WriteLine("=========================");
+                double B, H, Area;
+                Console.Write("Digite a base: ");
+                B = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Digite a altura: ");
+                H = Convert.ToDouble(Console.ReadLine());
+
+                Area = B * H;
+                Console.WriteLine("A área do terreno é : " + Area);
+
+            } while (true);
+        }
+    }
+}
